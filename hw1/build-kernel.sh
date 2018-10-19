10efedcccda2472fe055874e036182ff2a7f005a
Maksim Murygin <mail@muryginm.com>
2018-10-19 10:16:51 +0700

    hw1: fix provision script

--- hw1/build-kernel.sh
+++ hw1/build-kernel.sh
@@ -3,13 +3,15 @@
 declare -r VERSION=4.18.14
 
 yum update -y
-yum install -y wget \
+yum install -y \
+    wget \
     ncurses-devel \
     make \
     gcc \
     bc \
     bison \
     flex \
+    perl \
     elfutils-libelf-devel \
     openssl-devel \
     grub2
@@ -21,6 +23,8 @@ tar -xvf linux-"$VERSION".tar.xz
 cd linux-"$VERSION"
 cp /boot/config-"$(uname -r)" .config
 make olddefconfig
+make -j"$(nproc)" bzImage
+make -j"$(nproc)" modules
 make -j"$(nproc)"
-make install
 make modules_install
+make install
