fa3e9750139d3fff921d6ae43bb55236c6013ff9
Maksim Murygin <mail@muryginm.com>
2018-11-09 16:37:55 +0700

    hw5: add from/to report

--- hw5/process-logs.sh
+++ hw5/process-logs.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-echo "SOLO RUNNER"
+script_name=process-logs.sh
+
+from_date=$(grep "$script_name" < /var/log/cron | tail -1 | awk '{print $1 " " $2 " " $3}')
+to_date=$(date +"%b %d %H:%M:%S")
+
+echo "Report from $from_date to $to_date"
+
+echo "Some data..."
