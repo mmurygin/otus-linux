e9fee29085d13ee2348396c7ae72b65454bf3caf
Maksim Murygin <mail@muryginm.com>
2018-11-13 10:21:25 +0700

    hw4: implement volume group renaming

---
+++ hw4/rename-vg.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+declare -r old_name="VolGroup00"
+declare -r new_name="rootvg"
+
+vgrename -v $old_name $new_name
+sed -i "s/\/${old_name}-/\/${new_name}-/g" /etc/fstab
+sed -i "s/\([/=]\)${old_name}\([-/]\)/\1${new_name}\2/g" /boot/grub2/grub.cfg
+dracut -f -v "/boot/initramfs-$(uname -r).img" "$(uname -r)"
+systemctl reboot -f
