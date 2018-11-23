c6c99dfedd443d88a1a92fb9c1c372701ecedc14
Maksim Murygin <mail@muryginm.com>
2018-11-23 10:35:53 +0700

    hw6: add running spawn fcgi to setup script

--- hw6/fcgi/setup.sh
+++ hw6/fcgi/setup.sh
@@ -5,3 +5,5 @@ yum install -y epel-release
 yum install -y gcc gcc-c++ autoconf automake fcgi-devel spawn-fcgi
 
 gcc -o /usr/bin/test-server /vagrant/fcgi/test-server.c -lfcgi
+
+spawn-fcgi -p 3000 -f /usr/bin/test-server
