88776f264ff5dde2460b38326a11afdad2c67d5c
Murygin Maksim <mail@muryginm.com>
2018-11-27 22:51:53 +0700

    hw5: correct log processor

--- hw5/process-logs.sh
+++ hw5/process-logs.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-script_name=process-logs.sh
-
-from_date=$(grep "$script_name" < /var/log/cron | tail -1 | awk '{print $1 " " $2 " " $3}')
+from_date=$(grep "$0" < /var/log/cron | tail -1 | awk '{print $1 " " $2 " " $3}')
 to_date=$(date +"%b %d %H:%M:%S")
 
 echo "Report from $from_date to $to_date"
