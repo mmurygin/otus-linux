c686c64b7ac7aed8c63e4862a0754441f82ec964
Murygin Maksim <mail@muryginm.com>
2018-11-30 17:04:35 +0700

    hw8: implement custom rpm package and repo

--- hw8/provision.sh
+++ hw8/provision.sh
@@ -1,8 +1,44 @@
 #!/bin/bash
 
-# fix locale problem
-echo "export LC_ALL=en_US.utf-8" >> /etc/profile.d/locale.sh
-echo "export LANG=en_US.utf-8" >> /etc/profile.d/locale.sh
-mandb
+yum install -y \
+  redhat-lsb-core \
+  wget \
+  rpmdevtoools \
+  rpm-build \
+  createrepo \
+  yum-utils \
+  gcc
 
-yum install -y vim
+# get and extract nginx rpm source package
+wget https://nginx.org/packages/centos/7/SRPMS/nginx-1.14.1-1.el7_4.ngx.src.rpm
+rpm -i nginx-1.14.1-1.el7_4.ngx.src.rpm
+
+# install nginx dependencies
+yum-builddep /root/rpmbuild/SPECS/nginx.spec -y
+wget https://www.openssl.org/source/latest.tar.gz
+tar -xvf latest.tar.gz --directory /usr/lib
+
+# add openssl to nginx configuration options
+sed -i 's|--with-debug|--with-openssl=/usr/lib/openssl-1.1.1a|' /root/rpmbuild/SPECS/nginx.spec
+
+# build new nginx package with updated spec
+rpmbuild --bb /root/rpmbuild/SPECS/nginx.spec
+
+# install and configure nginx from newrly created rpm package
+yum localinstall -y /root/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm
+sed -i '/index  index.html index.htm;/a autoindex on;' /etc/nginx/conf.d/default.conf
+systemctl enable --now nginx
+
+# create rpm repo
+mkdir /usr/share/nginx/html/repo
+cp /root/rpmbuild/RPMS/x86_64/nginx-1.14.1-1.el7_4.ngx.x86_64.rpm /usr/share/nginx/html/repo/
+createrepo /usr/share/nginx/html/repo/
+
+# add rpm repo to available list
+cat >> /etc/yum.repos.d/custom.repo << EOF
+[custom]
+name=custom-repo
+baseurl=http://localhost/repo
+gpgcheck=0
+enabled=1
+EOF
