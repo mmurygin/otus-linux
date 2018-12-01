a273e242aa6aa05ffb381324fe60c66f711c2068
Murygin Maksim <mail@muryginm.com>
2018-12-01 13:14:05 +0700

    hw9: correct ionice demo

--- hw9/ionice/generate-file-with-ionice.sh
+++ hw9/ionice/generate-file-with-ionice.sh
@@ -2,12 +2,14 @@
 
 class="$1"
 priority="$2"
+filename="$(uuidgen).txt"
 
 START=$(date +%s.%N)
 
-ionice -c "$class" -n "$priority" dd if=/dev/zero of="$(uuidgen).txt" count=2048576 bs=1024
+ionice -c "$class" -n "$priority" -t dd if=/dev/zero of="$filename" count=2048576 bs=1024
+rm "$filename"
 
 END=$(date +%s.%N)
 DIFF=$(echo "$END - $START" | bc)
 
-echo "Running time of command with class ${class} and priority ${priority} ${DIFF} seconds"
+echo "Running time of command with class ${class} and priority ${priority}: ${DIFF} seconds"
--- hw9/ionice/run.sh
+++ hw9/ionice/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ./ionice/generate-file-with-ionice.sh 2 7 &
-./ionice/generate-file-with-ionice.sh 1 0 &
+./ionice/generate-file-with-ionice.sh 2 0 &
 
 wait
