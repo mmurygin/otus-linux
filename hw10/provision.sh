f4e695ed2e0ccdfe5e1fc7c6765e38d6c12c95ca
Murygin Maksim <mail@muryginm.com>
2018-12-03 15:57:22 +0700

    hw10: add star vagrant file

---
+++ hw10/provision.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# fix locale problem
+echo "export LC_ALL=en_US.utf-8" >> /etc/profile.d/locale.sh
+echo "export LANG=en_US.utf-8" >> /etc/profile.d/locale.sh
+mandb
+
+yum install -y vim
