4baae4e797ead88730841928f1f869da636f2c8c
Murygin Maksim <mail@muryginm.com>
2018-12-04 16:11:47 +0700

    hw11: install nginx with ansible provisioner

--- hw10/provision.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# fix locale problem
-echo "export LC_ALL=en_US.utf-8" >> /etc/profile.d/locale.sh
-echo "export LANG=en_US.utf-8" >> /etc/profile.d/locale.sh
-mandb
-
-yum install -y vim
