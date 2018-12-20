f99d15a97097f3ff0312d2b735dec553bac9af68
Maksim Murygin <mail@muryginm.com>
2018-12-20 09:37:17 +0700

    hw05: update log processor

--- hw05/process-logs.sh
+++ hw05/process-logs.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-from_date=$(grep "$0" < /var/log/cron | tail -1 | awk '{print $1 " " $2 " " $3}')
-to_date=$(date +"%b %d %H:%M:%S")
+LOG_FILE_NAME=/var/log/log-processor.log
+
+function get_current_date() {
+  date +"%b %d %H:%M:%S"
+}
+
+if [[ -e "$LOG_FILE_NAME" ]]; then
+  from_date=$(tail -1 "$LOG_FILE_NAME" | sed 's_\[\(.*\)\] Processng logs_\1_')
+fi
+
+to_date=$(get_current_date)
+
+echo "[$to_date] Processing Logs" >> "$LOG_FILE_NAME"
 
 echo "Report from $from_date to $to_date"
 
 echo "Some data..."
+
+echo "Last processed line: "
