ca298f9a3134368e8a3d132c361e4d2c17869795
Maksim Murygin <mail@muryginm.com>
2019-02-04 17:08:40 +0700

    hw22: correct startup template

--- hw22/README.md
+++ hw22/README.md
@@ -10,4 +10,20 @@
 4) пробросить 80й порт на inetRouter2 8080
 5) дефолт в инет оставить через inetRouter
 
+## Solution
+1. Run environment
+    ```bash
+    vagrant up
+    ./host/setup-ssh-tunnels.sh
+    ./host/disable-all-if-eth0.sh
+    ```
 
+### Check Results
+1. Connect to any vm
+    ```bash
+    ssh -i ~/.vagrant.d/insecure_private_key -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null vagrant@127.0.0.1 -p port
+    ```
+
+    * ports mapping:
+        * centralRouter: 8001
+        * centralServer: 8002
---
+++ hw22/host/setup-ssh-tunnels.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo IMPORTANT: Please make sure that ports 8001-8002 are opened
+
+private_key="${HOME}/.vagrant.d/insecure_private_key"
+ssh_options="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+echo Forwarding centralRouter to localhost:8001
+ssh -i "$private_key" -L 8001:192.168.255.2:22 $ssh_options -p 2222 -N vagrant@127.0.0.1 &
+sleep 1
+
+echo Forwarding centralServer to localhost:8002
+ssh -i "$private_key" -L 8002:192.168.0.2:22 $ssh_options -p 8001 -N vagrant@127.0.0.1 &
+sleep 1
+
+echo "Done"
